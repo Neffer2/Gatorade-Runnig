b79e35a7545bf033aa3337a6363eff5f4003b3f7
Desarrollador Iglu <n3f73r@gmail.com>
2023-06-09 09:17:49 -0500

    botones compartir pruebas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,36 +1,4 @@
 @extends('layouts.main')
-<style>
-    .share-buttons {
-       display: flex;
-       gap: 10px;
-    }
-    .share-button {
-       border: none;
-       border-radius: 5px;
-       color: #fff;
-       cursor: pointer;
-       font-size: 16px;
-       padding: 8px 16px;
-    }
-    .facebook {
-       background-color: #3b5998;
-    }
-    .twitter {
-       background-color: #1da1f2;
-    }
-    .linkedin {
-       background-color: #0077b5;
-    }
-    .pinterest {
-       background-color: #bd081c;
-    }
-    .reddit {
-       background-color: #ff4500;
-    }
-    .whatsapp {
-       background-color: #25d366;
-    }
- </style>
     @section('content')
         Deten el tiepo en tu mano
         @php 
@@ -44,67 +12,10 @@
             @csrf
             <button>Salir</button>
         </form>
-    @endsection 
-    @section('scripts') 
-    <script>
-        // Crear el lienzo (canvas)
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
 
-        // Crear la imagen original
-        const originalImage = new Image();
-        originalImage.src = "{{ asset("storage/$aux") }}";
-
-        // Crear el logo
-        const logo1 = new Image();
-        logo1.src = "{{ asset('pruebas/logo.png') }}";
-
-        const logo2 = new Image();
-        logo2.src = "{{ asset('pruebas/logo.png') }}";
-        
-        const logo1Width = 200; // Anchura deseada del logo
-        const logo1Height = 73; // Altura deseada del logo
-
-        const logo2Width = 200; // Anchura deseada del logo
-        const logo2Height = 73; // Altura deseada del logo
-
-        // Cuando tanto la imagen original como el logo se hayan cargado
-        Promise.all([
-            new Promise((resolve) => originalImage.onload = resolve),
-            new Promise((resolve) => logo1.onload = resolve),
-            new Promise((resolve) => logo2.onload = resolve)
-        ]).then(() => {
-        // Establecer el tamaño del lienzo al tamaño de la imagen original
-        canvas.width = originalImage.width;
-        canvas.height = originalImage.height;
-
-        // Dibujar la imagen original en el lienzo
-        ctx.drawImage(originalImage, 0, 0);
-
-        // Dibujar el logo en una posición específica
-        const logo1X = 5 ; // Ajusta la posición del logo en el eje X
-        const logo1Y = canvas.height - (logo1Height + 10); // Ajusta la posición del logo en el eje Y
-        ctx.drawImage(logo1, logo1X, logo1Y, logo1Width, logo1Height);
-
-        // Dibujar el logo en una posición específica
-        const logo2X = canvas.width - (logo2Width + 5) ; // Ajusta la posición del logo en el eje X
-        const logo2Y = canvas.height - (logo2Height + 10); // Ajusta la posición del logo en el eje Y
-        ctx.drawImage(logo2, logo2X, logo2Y, logo2Width, logo2Height);
-
-        // Obtener la imagen resultante con el logo
-        const imageWithLogo = new Image();
-        imageWithLogo.src = canvas.toDataURL();
-
-        // Agregar la imagen resultante al documento
-        // console.log();
-        // document.body.appendChild(imageWithLogo);
-        document.getElementById('imagen').src = imageWithLogo.src;
-        document.getElementById('image-download').href = imageWithLogo.src;
-        });
-    </script>
-     <div class="share-buttons">
-        <button class="share-button facebook">
-           <i class="fab fa-facebook-f"></i>
+        <div class="share-buttons">
+         <button class="share-button facebook">
+            <i class="fab fa-facebook-f"></i>
         </button>
         <button class="share-button twitter">
            <i class="fab fa-twitter"></i>
@@ -121,8 +32,48 @@
         <button class="share-button whatsapp">
            <i class="fab fa-whatsapp"></i>
         </button>
-     </div>
+      </div>
+    @endsection 
+    @section('scripts') 
+    <script>
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const originalImage = new Image();
+        originalImage.src = "{{ asset("storage/$aux") }}";
 
+        const logo1 = new Image();
+        logo1.src = "{{ asset('logos/logo.png') }}";
+        const logo2 = new Image();
+        logo2.src = "{{ asset('logos/logo.png') }}";      
+        const logo1Width = 200;
+        const logo1Height = 73;
+        const logo2Width = 200;
+        const logo2Height = 73;
+      
+        Promise.all([
+            new Promise((resolve) => originalImage.onload = resolve),
+            new Promise((resolve) => logo1.onload = resolve),
+            new Promise((resolve) => logo2.onload = resolve)
+        ]).then(() => {
+        canvas.width = originalImage.width;
+        canvas.height = originalImage.height;
+        ctx.drawImage(originalImage, 0, 0);
+        const logo1X = 5 ;
+        const logo1Y = canvas.height - (logo1Height + 10);
+        ctx.drawImage(logo1, logo1X, logo1Y, logo1Width, logo1Height);
+
+        const logo2X = canvas.width - (logo2Width + 5);
+        const logo2Y = canvas.height - (logo2Height + 10);
+        ctx.drawImage(logo2, logo2X, logo2Y, logo2Width, logo2Height);
+
+        const imageWithLogo = new Image();
+        imageWithLogo.src = canvas.toDataURL();
+
+        document.getElementById('imagen').src = imageWithLogo.src;
+        document.getElementById('image-download').href = imageWithLogo.src;
+        });
+    </script>
+    
      <script>
         // Get all share buttons
         const shareButtons = document.querySelectorAll('.share-button');
@@ -137,22 +88,22 @@
               let shareUrl;
               switch (platform) {
                  case 'facebook':
-                 shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+                 shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${document.getElementById('imagen').src}`;
                  break;
                  case 'twitter':
-                 shareUrl = `https://twitter.com/share?url=${encodeURIComponent(url)}`;
+                 shareUrl = `https://twitter.com/share?url=${document.getElementById('imagen').src}`;
                  break;
                  case 'linkedin':
-                 shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`;
+                 shareUrl = `https://www.linkedin.com/shareArticle?url=${document.getElementById('imagen').src}`;
                  break;
                  case 'pinterest':
-                 shareUrl = `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}`;
+                 shareUrl = `https://pinterest.com/pin/create/button/?url=${document.getElementById('imagen').src}`;
                  break;
                  case 'reddit':
-                 shareUrl = `https://reddit.com/submit?url=${encodeURIComponent(url)}`;
+                 shareUrl = `https://reddit.com/submit?url=${document.getElementById('imagen').src}`;
                  break;
                  case 'whatsapp':
-                 shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
+                 shareUrl = `https://api.whatsapp.com/send?text=${document.getElementById('imagen').src}`;
                  break;
               }
               // Open a new window to share the URL
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
+    <script src="https://kit.fontawesome.com/15bc5276a1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
     <title>Document</title>
     @livewireStyles
 </head> 
